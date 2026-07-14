51da6ded85cfc6b232790841776660b32f94f104
gustavo <gucerni@gmail.com>
2026-07-13 20:30:46 -0400

    se crean bloques azules mediante un array. Los bloques tienen rigidbody y box collider. La pelota solo puede rebotar en estos bloques, falta la logica para que sean destruidos

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class gameManager : MonoBehaviour
 {
     public GameObject pelotaPrefab;
+    public GameObject ladrilloAzul;
+
+    private float[] bloqueAzul = {-5f, -3f, -1f, 1f, 3f, 5f}; 
+
     void Start()
     {
         SpawnPelota();
+        CreaLadrilloAzul();
     }
 
     // Update is called once per frame
@@ -17,5 +22,11 @@ public class gameManager : MonoBehaviour
     //crea una pelota al inicio y luego de cada gol
     public void SpawnPelota(){
         Instantiate(pelotaPrefab, new Vector2(0,-3.65f), Quaternion.identity);
+    }
+
+    public void CreaLadrilloAzul(){
+        for(int i = 0; i < bloqueAzul.Length; i++){
+            Instantiate(ladrilloAzul, new Vector2(bloqueAzul[i], 2.14f), Quaternion.identity);
+        }
     }
 }
