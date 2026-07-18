80042b3724dac22c1829625eb3c3ffc7e6af1936
gustavo <gucerni@gmail.com>
2026-07-17 23:35:49 -0400

    Se implementa cronometro que al llegar a cero, da gameover

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -8,6 +8,7 @@ public class gameManager : MonoBehaviour
     public GameObject ladrilloAzul;
     public GameObject ladrilloVerde;
     public GameObject ladrilloRojo;
+    public GameObject paletaPlayer;
     public paleta posicionPaleta; //debe tener el mismo nombre que el script para poder referenciar
     public UIDocument uiDocument;
     
@@ -16,10 +17,18 @@ public class gameManager : MonoBehaviour
     private Label finDelJuego;
     private Label continuarJuego;
     private Label puntaje;
+    private Label cronometro;
+    private Label sinTiempo;
+    private Label victoria;
     private Button restartButton;
+    private GameObject pelotaActual;
+
     private bool esperandoInput = false;
+    private bool timerActivo = true;
+    private float tiempoRestante = 300f; //tiempo del cronometro
     private int puntajeTotal = 0;
     private int cantidadVidas = 3;
+
     
     private float[] bloquePosX = {-5f, -3f, -1f, 1f, 3f, 5f}; 
 
@@ -27,18 +36,26 @@ public class gameManager : MonoBehaviour
     {
         SpawnPelota();
         CreaLadrillo();
+
         scoreText1 = uiDocument.rootVisualElement.Q<Label>("puntaje");
         vidasRestantes = uiDocument.rootVisualElement.Q<Label>("vidas");
         finDelJuego = uiDocument.rootVisualElement.Q<Label>("gameOver");
         continuarJuego = uiDocument.rootVisualElement.Q<Label>("continuar");
-        finDelJuego.style.display = DisplayStyle.None;
-        continuarJuego.style.display = DisplayStyle.None;
+        cronometro = uiDocument.rootVisualElement.Q<Label>("tiempo");
+        sinTiempo = uiDocument.rootVisualElement.Q<Label>("sinTiempo");
+        victoria = uiDocument.rootVisualElement.Q<Label>("victoria");
         restartButton = uiDocument.rootVisualElement.Q<Button>("reiniciar");
-        restartButton.style.display = DisplayStyle.None;
         restartButton.clicked += ReloadScene;
+        
+        //Oculta elementos de la interfaz, como pantalla de gameover y boton restart
+        finDelJuego.style.display = DisplayStyle.None;
+        sinTiempo.style.display = DisplayStyle.None;
+        continuarJuego.style.display = DisplayStyle.None;
+        victoria.style.display = DisplayStyle.None;
+        restartButton.style.display = DisplayStyle.None;
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
          if (esperandoInput && Input.GetKeyDown(KeyCode.Space) && cantidadVidas > 0){
@@ -46,30 +63,78 @@ public class gameManager : MonoBehaviour
             esperandoInput = false;
             continuarJuego.style.display = DisplayStyle.None;
         }
+
+        //############## PARA EL CRONOMETRO ################
+        if (timerActivo && cantidadVidas > 0)
+        {
+            tiempoRestante -= Time.deltaTime;
+            
+            // Actualizar el texto en pantalla
+            int minutos = Mathf.FloorToInt(tiempoRestante / 60f);
+            int segundos = Mathf.FloorToInt(tiempoRestante % 60f);
+            cronometro.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+            
+            // Verificar si el tiempo se acabó
+            if (tiempoRestante <= 0f)
+            {
+                tiempoRestante = 0f;
+                timerActivo = false;
+                cantidadVidas = 0;
+                GameOver();
+            }
+        }
+        //##################################################
     }
 
-    // Este método es llamado por la pelota cuando cae al vacío
-    public void PelotaPerdida()
-    {
+    //########  PARA MANEJAR LA CANTIDAD DE VIDAS ##########
+    public void PelotaPerdida(){
         esperandoInput = true;
         cantidadVidas--;
         vidasRestantes.text = cantidadVidas.ToString();
         
         if(cantidadVidas < 1){
-            finDelJuego.style.display = DisplayStyle.Flex;
-            restartButton.style.display = DisplayStyle.Flex;
+            GameOver();
         }
         if(cantidadVidas > 0){
             continuarJuego.style.display = DisplayStyle.Flex;
         }
     }
+    //######################################################
 
-    //crea una pelota al inicio y luego de cada gol
-    public void SpawnPelota(){
-        float posicionX = posicionPaleta.ObtenerPosicionX(); //obtenemos la posicion x de la paleta
-        Instantiate(pelotaPrefab, new Vector2(posicionX,-3.65f), Quaternion.identity);
+
+    //##########  PARA LA PANTALLA GAME OVER #############
+    public void GameOver(){
+        finDelJuego.style.display = DisplayStyle.Flex;
+        restartButton.style.display = DisplayStyle.Flex;
+
+        if(tiempoRestante <= 0){
+            sinTiempo.style.display = DisplayStyle.Flex;
+        }
+        
+        cronometro.text = "00:00";
+        vidasRestantes.text = "0";
+        
+        if (pelotaActual != null)
+        {
+            Destroy(pelotaActual);
+            pelotaActual = null;
+        }
     }
+    //######################################################
 
+
+    //###########  PARA LA CREACION DE LA PELOTA ###########
+    public void SpawnPelota(){
+        //obtenemos la posicion x de la paleta
+        float posicionX = posicionPaleta.ObtenerPosicionX(); 
+
+        //crea una pelota y entrega una referencia para destruirla al acabar el tiempo
+        pelotaActual = Instantiate(pelotaPrefab, new Vector2(posicionX,-3.65f), Quaternion.identity);
+    }
+    //######################################################
+
+
+    //###########  PARA LA CREACION DE LADRILLOS ###########
     public void CreaLadrillo(){
         for(int i = 0; i < bloquePosX.Length; i++){
             Instantiate(ladrilloAzul, new Vector2(bloquePosX[i], 2.14f), Quaternion.identity);
@@ -77,24 +142,47 @@ public class gameManager : MonoBehaviour
             Instantiate(ladrilloRojo, new Vector2(bloquePosX[i], 3.62f), Quaternion.identity);
         }
     }
+    //######################################################
+
+
+    //##############  CONTROLA EL PUNTAJE ##################
 
     //controla el puntaje de cada ladrillo
     public void PuntoLadrilloAzul(){
         puntajeTotal += 10;
         scoreText1.text = puntajeTotal.ToString();
+        if(puntajeTotal == 360){
+            Victoria();
+        }
     }
     public void PuntoLadrilloVerde(){
         puntajeTotal +=  20;
         scoreText1.text = puntajeTotal.ToString();
+        if(puntajeTotal == 360){
+            Victoria();
+        }
     }
     public void PuntoLadrilloRojo(){
         puntajeTotal +=  30;
         scoreText1.text = puntajeTotal.ToString();
+        if(puntajeTotal == 360){
+            Victoria();
+        }
     }
-
+    //######################################################
+    
     //reinicia el juego
     void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Victoria(){
+        if (pelotaActual != null){
+            Destroy(pelotaActual);
+            Destroy(paletaPlayer);
+            pelotaActual = null;
+        }
+        victoria.style.display = DisplayStyle.Flex;
     }
 }
